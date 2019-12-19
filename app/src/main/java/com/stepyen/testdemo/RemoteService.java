8e71b4901dcfc70a471b84c309e0f648f23f77e8
stepyen <stepyen@163.com>
2019-12-19 18:42:37 +0800

    【增加】 1、AIDL学习-服务端实现

--- app/src/main/java/com/stepyen/testdemo/RemoteService.java
+++ app/src/main/java/com/stepyen/testdemo/RemoteService.java
@@ -3,16 +3,36 @@ package com.stepyen.testdemo;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
+import com.stepyen.testdemo.aidl.RemoteInterface;
 
 /**
- * date：2019-12-17
+ * date：2019-12-18
  * author：stepyen
  * description：
  */
 public class RemoteService extends Service {
 
+    public RemoteService() {
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
+
+    private final RemoteInterface.Stub mBinder = new RemoteInterface.Stub() {
+        @Override
+        public void setString(String str) throws RemoteException {
+        }
+
+        @Override
+        public String getString() throws RemoteException {
+            return "成功调用远程服务";
+        }
+    };
 }
+
+
+
+
